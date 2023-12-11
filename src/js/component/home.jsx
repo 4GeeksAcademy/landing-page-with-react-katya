6e2fd9ca17305598f79ae154b95247fb2f795da7
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-12-11 22:06:19 +0000

    Structure is done without the footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Jumbotron from "./jumbotron";
+import Navbar from "../navbar";
+import Card from "./card";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <div class="d-flex justify-content-center">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
